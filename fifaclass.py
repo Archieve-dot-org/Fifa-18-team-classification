61f27281ce859adb9104299bed2a948abc40de6a
Shaon Majumder <shaon.fdr@robist.com>
2018-06-02 10:19:14 +0600

    directory creation

--- fifaclass.py
+++ fifaclass.py
@@ -1,10 +1,13 @@
-import re
+import re,os
+
 with open('Fifa 18.txt','r') as f:
 	output = f.read()
 	group_name = ( (  (re.sub('".*?"', '', output)).replace(" ", "")  ).replace("=","") ).splitlines()
 	del group_name[-1]
 
-group_coun = re.findall(r'"(.*?)"', output)
+group_coun = re.findall(r'"(.*?)"', output.lower())
 dictionary = dict(zip(group_name, group_coun))
-print dictionary
-	
+
+for directory in dictionary:
+	if not os.path.exists(directory):
+		os.makedirs(directory)
