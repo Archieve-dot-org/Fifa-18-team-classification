d00aba389da92dd145789d521d142455d3bd48e6
Shaon Majumder <shaon.fdr@robist.com>
2018-06-02 11:29:21 +0600

    gather results in result folder

--- fifaclass.py
+++ fifaclass.py
@@ -11,16 +11,16 @@ with open('Fifa 18.txt','r') as f:
 group_coun = re.findall(r'"(.*?)"', output.lower())
 dictionary = dict(zip(group_name, group_coun))
 
-for sc, value in dictionary.iteritems():
-	#print dictionary[directory]
-	
-	if os.path.exists(sc):
-		shutil.rmtree(sc)
-		
-	os.makedirs(sc)	
+def folder_create(dirs):
+	if os.path.exists(dirs):
+		shutil.rmtree(dirs)
+	os.makedirs(dirs)
 
+folder_create('result')
+for sc, value in dictionary.iteritems():
+	folder_create('result/'+sc)
 
 	for scd in value.split(','):
 		for file in glob.glob('svg/*'+scd+'*'):
 			print file
-			shutil.copy(file, sc)
+			shutil.copy(file, 'result/'+sc)
